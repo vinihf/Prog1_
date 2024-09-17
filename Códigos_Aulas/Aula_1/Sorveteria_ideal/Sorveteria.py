647e76e5e8ea2732dbd9254f388fc83914103c53
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2024-09-16 21:26:13 -0300

    DocStrings

--- Códigos_Aulas/Aula_1/Sorveteria_ideal/Sorveteria.py
+++ Códigos_Aulas/Aula_1/Sorveteria_ideal/Sorveteria.py
@@ -1,3 +1,17 @@
+'''
+
+Docstrings
+
+https://gist.github.com/nipunsadvilkar/fec9d2a40f9c83ea7fd97be59261c400
+'''
+
 def calcula_preco_com_retorno(quantidade:int = 1, preco_unitario:float = 5.5):
+    """
+    Função para calcular o preço da compra de sorvetes
+
+    :param quantidade: quantidade de sorvetes
+    :param preco_unitario: preço de um sorvete
+    :return: preço da compra
+    """
     preco = quantidade * preco_unitario
     return preco
