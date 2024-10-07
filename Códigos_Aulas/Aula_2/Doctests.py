bab2824973eb6c1c084cd5847216f31bcce3f658
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2024-10-07 14:56:54 -0300

    Testes

---
+++ Códigos_Aulas/Aula_2/Doctests.py
@@ -0,0 +1,27 @@
+"""
+
+Da mesma forma que podemos usar o padrão Docstrings para documentar as funções que escrevemos
+há um padrão para realizar testes das nossas funções, o padrão doctests
+
+https://docs.python.org/3/library/doctest.html
+"""
+
+def somar(valor1:int, valor2:int) -> int:
+    """
+    Calcula a soma de dois valores
+    Args:
+        valor1: primeiro valor(int)
+        valor2: segundo valor(int)
+
+    Returns: soma dos dois valores(int)
+
+    >>> somar(2,4)
+    6
+    """
+    return valor1 + valor2
+
+def main():
+    print(somar(2,4))
+
+if __name__ == '__main__':
+    main()
--- Códigos_Aulas/Aula_2/Resumo_Repetições.py
+++ Códigos_Aulas/Aula_2/Resumo_Repetições.py
@@ -3,7 +3,7 @@ https://allendowney.github.io/ThinkPython/chap03.html
 
 O uso de funções possui dois grandes objetivos:
 1 - Agrupar funcionalidades em pequenos módulos
-2 - Reaproveitamento de código
+2 - Reaproveitar código
 
 Vamos tentar fazer um teste com a música "Sujeito de sorte", do cantor Belchior.
 https://www.youtube.com/watch?v=mGAra8tXeJc
@@ -39,13 +39,29 @@ def refrao()->None:
     print("Ano passado eu morri, mas esse ano eu não morro")
 
 def main():
-    verso()
+    '''verso()
     print("\n")
     refrao()
     print("\n")
     verso()
     print("\n")
     refrao()
+    '''
+
+    '''
+    E se pudéssemos repetir um determinado número de vezes um conjunto de códigos?
+    
+    Para isso existem os comandos de repetição. 
+    
+    O primeiro que iremos estudar é o comando for.
+    
+    '''
+
+    for i in range(3):
+        verso()
+        print("\n")
+        refrao()
+        print("\n")
 
 if __name__ == '__main__':
     main()
