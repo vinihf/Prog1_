1a23a6f45587a487ec5157d1312ccb85a3126a93
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2025-06-27 13:36:03 -0300

    Update main.py

--- Códigos_Aulas/Turtle/main.py
+++ Códigos_Aulas/Turtle/main.py
@@ -1,14 +1,25 @@
 import turtle
 
-t = turtle.Turtle()
-t.pencolor("blue")
-t.pensize(3)
-t.fillcolor("yellow")
+def quadradoPreenchido():
+    t = turtle.Turtle()
+    t.pencolor("blue")
+    t.pensize(3)
+    t.fillcolor("yellow")
+    t.begin_fill()
+    for i in range(4):
+        t.forward(100)
+        t.left(90)
+    t.end_fill()
+    turtle.done()
 
-t.begin_fill()
-for _ in range(4):
-    t.forward(100)
-    t.left(90)
-t.end_fill()
+def circuloPreenchido():
+    t = turtle.Turtle()
+    t.pencolor("blue")
+    t.pensize(3)
+    t.fillcolor("yellow")
+    t.begin_fill()
+    t.circle(100)
+    t.end_fill()
+    turtle.done()
 
-turtle.done()
+circuloPreenchido()
